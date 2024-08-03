c09c81405b5c0a2756a1916ccfdbf2e26f9cdc19
aortegaf <andres.ortega@puntored.co>
2024-08-02 20:04:12 -0500

    arrow functions

---
+++ basics/arrow_functions.js
@@ -0,0 +1,28 @@
+// Traditional function
+const traditionalGreeting = function (name) {
+    return `Hi ${name}`;
+} 
+
+// Arrow function, explicit return
+const arrowGreetingExplicit = (name) => {return `Hi ${name}`}
+
+// Arrow function, implicit return
+const arrowGreetingImplicit = name => {return `Hi ${name}`}
+const arrowGreetingImplicitMultipleParam = (name, lastName) => {return `Hi ${name} ${lastName}`}
+
+console.log(traditionalGreeting("Batman"));
+console.log(arrowGreetingExplicit("Batman"));
+console.log(arrowGreetingImplicit("Batman"));
+console.log(arrowGreetingImplicitMultipleParam("Bruce", "Wayne"));
+
+//Lexical binding
+const fictionalCharacter = {
+    name: "Uncle Ben",
+    traditionalMessage: function (message) {
+        console.log(`${this.name} says: ${message}`)
+    },
+    arrowMessage: (message) => console.log(`${this.name} says: ${message}`)
+}
+
+fictionalCharacter.traditionalMessage("With great power comes great responsability");
+fictionalCharacter.arrowMessage("Beware of Doctor Octopus");
